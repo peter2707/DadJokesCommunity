5e66c379779022b054f263b9344611f6185262e4
prumm <prumm@PETER>
2024-03-09 18:54:56 +1100

    Add project files.

---
+++ DadJokesCommunity/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DadJokesCommunity.Models;
+
+namespace DadJokesCommunity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DadJokesCommunity.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ DadJokesCommunity/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace DadJokesCommunity.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
